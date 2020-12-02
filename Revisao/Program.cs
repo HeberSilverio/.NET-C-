72eff235965a5e96e0cbdf5aed3fd845ef62b5ea
Héber Silvério <hebersilverio@gmail.com>
2020-12-02 19:24:39 -0300

    cria aplicação revisão

---
+++ Revisao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opçãoUsuario = ObterOpcaoUsuario();
+
+            while (opçãoUsuario.ToUpper() != "X")
+            {
+                switch (opçãoUsuario)
+                {
+                    case "1":
+                        //TODO: Adicionar aluno
+
+                        break;
+                    case "2":
+                        //TODO: Adicionar aluno
+                        break;
+                    case "3":
+                        //TODO: Adicionar aluno
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                opçãoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar aluno");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opçãoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opçãoUsuario;
+        }
+    }
+}
